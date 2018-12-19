457583de7917072e8c9391aff95439abf5551143
Ludanark <ludanarks@gmail.com>
2018-12-19 19:42:19 +0800

    更新数据库user的密码 加入登录拦截器 加入登录后的拦拦截器

--- blog-web-admin/src/main/java/com/gw/blog/web/admin/web/interceptor/LoginInterceptor.java
+++ blog-web-admin/src/main/java/com/gw/blog/web/admin/web/interceptor/LoginInterceptor.java
@@ -1,10 +1,30 @@
 package com.gw.blog.web.admin.web.interceptor;
 
+import com.gw.blog.commons.contants.Contents;
+import com.gw.blog.domain.User;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 登陆拦截器
  *
  */
 public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        User user = (User) request.getSession().getAttribute(Contents.SESSION_USER);
+
+        // 用户未登录
+        if (user == null) {
+            // 重定向到登录页
+            response.sendRedirect("/back/login");
+            return false;
+        }
+
+        // 用户已登录
+        return true;
+    }
 }
--- blog-web-admin/src/main/java/com/gw/blog/web/admin/web/interceptor/MainInterceptor.java
+++ blog-web-admin/src/main/java/com/gw/blog/web/admin/web/interceptor/MainInterceptor.java
@@ -1,10 +1,26 @@
 package com.gw.blog.web.admin.web.interceptor;
 
 
+import com.gw.blog.commons.contants.Contents;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 登陆后的拦截器
  */
 public class MainInterceptor implements HandlerInterceptor {
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        // 用户已登录
+        if (request.getSession().getAttribute(Contents.SESSION_USER) != null) {
+            // 如果请求来自登录页
+            if (modelAndView.getViewName().endsWith("login") && modelAndView != null) {
+                // 重定向到后台首页
+                response.sendRedirect("/back/main");
+            }
+        }
+    }
 }
