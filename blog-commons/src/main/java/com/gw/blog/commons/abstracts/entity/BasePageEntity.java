75c9c49b3fed2fad55dd8acca919487c55177808
Ludanark <ludanarks@gmail.com>
2018-12-19 21:56:06 +0800

    修改登录功能

--- blog-commons/src/main/java/com/gw/blog/commons/abstracts/entity/BasePageEntity.java
+++ blog-commons/src/main/java/com/gw/blog/commons/abstracts/entity/BasePageEntity.java
@@ -2,8 +2,10 @@ package com.gw.blog.commons.abstracts.entity;
 
 import com.gw.blog.commons.dto.Page;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class BasePageEntity extends BaseEntity {
     /**
      * 分页
