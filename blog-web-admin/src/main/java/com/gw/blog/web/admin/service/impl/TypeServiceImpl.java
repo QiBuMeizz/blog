2c8ffed9a86a49beffaa2e94c0d8bdf5283a1bf0
Ludanark <ludanarks@gmail.com>
2018-12-23 17:19:22 +0800

    分类管理： 1.修复添加下级分类时，父类 isParent 值不变的问题 2.修复一级分类个数无限制问题
    分类管理功能有：
    1.新增/修改
    1.1.一级父类限制最大个数为8
    1.2.当有子类时，本类不可成为子类
    1.3.本类不可成为本类的父类
    1.4.新增或修改时，其父类的 isParent 值要更变
    1.5.分类深度只有二级
    
    2.删除
    2.1.多表删除--删除分类及其子类和它们所属所有文章

--- blog-web-admin/src/main/java/com/gw/blog/web/admin/service/impl/TypeServiceImpl.java
+++ blog-web-admin/src/main/java/com/gw/blog/web/admin/service/impl/TypeServiceImpl.java
@@ -97,17 +97,21 @@ public class TypeServiceImpl extends BaseTreeServiceImpl<Type, TypeDao> implemen
     public String check(Type type) {
         // 本类ID
         Long id = type.getId();
+        // 原类别
+        Type oldType = selectById(id);
         // 改变后的 parentId
         Long parentId = type.getParentId();
         // 改变后的父类
         Type parent = selectById(parentId);
+        // 一级类目个数
+        int size1 = getByParentId(0L).size();
+        // 二级类目个数
+        int size2 = getByParentId(id).size();
         // 新增
         if (id == null) {
             // 无父类或父类无父类
             if (parentId == 0L || parent.getParentId() == 0L) {
-                // 限制父类个数
-                int size = getByParentId(0L).size();
-                if ( size < 8) {
+                if ( size1 < 8 || parentId != 0L) {
                     // 改变父类 isParent 值
                     turnIsParent(parent, null);
                     return null;
@@ -121,15 +125,12 @@ public class TypeServiceImpl extends BaseTreeServiceImpl<Type, TypeDao> implemen
 
         // 更新
         else {
-
-
             // 本类不可为自身类的父类
             if (id != parentId) {
                 // 无父类或父类无父类
                 if (parentId == 0L || parent.getParentId() == 0L) {
-                    // 原类别
-                    Type oldType = selectById(id);
-                    if (oldType.getParentId() != 0L) {
+                    // 子类最多只有一个
+                    if (size2 <= 1 && size1 < 8) {
                         // 改变父类 isParent 值
                         turnIsParent(parent, oldType);
                         return null;
@@ -144,10 +145,10 @@ public class TypeServiceImpl extends BaseTreeServiceImpl<Type, TypeDao> implemen
         return "此分类不能成为父类";
     }
 
-
     /**
      * 改变父类 isParent 值
-     * @param parent
+     * @param parent 新父类
+     * @param oldType  原分类
      */
     private void turnIsParent(Type parent, Type oldType) {
 
@@ -155,7 +156,7 @@ public class TypeServiceImpl extends BaseTreeServiceImpl<Type, TypeDao> implemen
         if (parent != null) {
             if (!parent.getIsParent()) {
                 parent.setIsParent(true);
-                save(parent);
+                dao.update(parent);
             }
         }
 
@@ -167,7 +168,7 @@ public class TypeServiceImpl extends BaseTreeServiceImpl<Type, TypeDao> implemen
             if (getByParentId(oldParentId).size() == 1) {
                 Type oldParent = selectById(oldParentId);
                 oldParent.setIsParent(false);
-                save(oldParent);
+                dao.update(oldParent);
             }
         }
     }
